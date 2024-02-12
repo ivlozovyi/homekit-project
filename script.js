200eac7f0479183711d46c8a3e5cfe6b886b1024
Vebster <vebster2580@gmail.com>
2024-02-12 21:49:34 +0200

    fully added lights and humidity tabs

--- script.js
+++ script.js
@@ -13,6 +13,14 @@ const dom = {
     temperatureButton: document.querySelector('.temperature-button'),
     temperatureSaveButton: document.querySelector('.set-button'),
     temperaturePowerButton: document.querySelector('.power'),
+    sliders: {
+        lights: document.querySelector('#lights-slider'),
+        humidity: document.querySelector('#humidity-slider'),
+    },
+    swtch: {
+        lights: document.getElementById('lights-power'),
+        humidity: document.getElementById('humidity-power'),
+    }
 
 }
 
@@ -30,45 +38,57 @@ const roomsData = {
     livingroom: {
         temperature: 16,
         temperatureOff: false,
-        lights: 0,
-        humidity: 0,
+        lights: 100,
+        lightsOff: false,
+        humidity: 80,
+        humidityOff: false,
     },
     bedroom: {
         temperature: 16,
         temperatureOff: false,
-        lights: 0,
-        humidity: 0,
+        lights: 100,
+        lightsOff: false,
+        humidity: 80,
+        humidityOff: false,
     },
     kitchen: {
         temperature: 16,
         temperatureOff: false,
-        lights: 0,
-        humidity: 0,
+        lights: 100,
+        lightsOff: false,
+        humidity: 80,
+        humidityOff: false,
     },
     bathroom: {
         temperature: 16,
         temperatureOff: false,
-        lights: 0,
-        humidity: 0,
+        lights: 100,
+        lightsOff: false,
+        humidity: 80,
+        humidityOff: false,
     },
     studio: {
         temperature: 16,
         temperatureOff: false,
-        lights: 0,
-        humidity: 0,
+        lights: 100,
+        lightsOff: false,
+        humidity: 80,
+        humidityOff: false,
     },
     washingroom: {
         temperature: 16,
         temperatureOff: false,
-        lights: 0,
-        humidity: 0,
+        lights: 100,
+        lightsOff: false,
+        humidity: 80,
+        humidityOff: false,
     },
 }
 
 let activeRoom = 'all';
 let activeTab = 'temperature';
 
-const { selectbox, selectboxList, selectboxSelected, rooms, selectboxItem, settings, settingsPanel, settingsTabs, temperatureLine, temperatureRound, temperatureInfo, temperatureButton, temperatureSaveButton, temperaturePowerButton } = dom;
+const { selectbox, selectboxList, selectboxSelected, rooms, selectboxItem, settings, settingsPanel, settingsTabs, temperatureLine, temperatureRound, temperatureInfo, temperatureButton, temperatureSaveButton, temperaturePowerButton, sliders, swtch } = dom;
 
 selectboxSelected.addEventListener('click', (e) => {
     selectbox.classList.toggle("open");
@@ -98,13 +118,18 @@ function selectRoom(room) {
     };
     if (room !== 'all') {
         const newSelectedRoom = rooms.querySelector(`[data-room=${room}`);
-        const temperature = roomsData[room].temperature;
+        const { temperature, lights, humidity, lightsOff, humidityOff } = roomsData[room];
         activeRoom = room;
         newSelectedRoom.classList.add('selected');
         renderScreen(false);
         temperatureInfo.innerText = temperature;
         renderTemperature(temperature);
         setTemperaturePower();
+        changeSettigsType(activeTab);
+        changeSlider(lights, sliders.lights);
+        changeSlider(humidity, sliders.humidity);
+        changeSwitch('lights', lightsOff)
+        changeSwitch('humidity', humidityOff)
     } else {
         renderScreen(true);
     }
@@ -114,7 +139,7 @@ function selectRoom(room) {
     newSelectedItem.classList.add('selected');
     const selectboxSpan = selectbox.querySelector('.selectbox__selected span');
     selectboxSpan.innerText = listOFRooms[room];
-    
+
 }
 
 rooms.querySelectorAll('.mobile__room').forEach(element => {
@@ -169,22 +194,22 @@ function changeTemperature() {
     let position = 0;
     let range = 0;
     let change = 0;
-    temperatureButton.onmouseover = () => mouseover = true;
+    temperatureButton.onmouseover = () =>{ mouseover = true;
+    mousedown = false;};
     temperatureButton.onmouseout = () => mouseover = false;
     temperatureButton.onmouseup = (e) => mousedown = false;
     temperatureButton.onmousedown = (e) => {
         mousedown = true;
         position = e.offsetY;
-        console.log(position);
         range = 0;
     }
     temperatureButton.onmousemove = (e) => {
         if (mouseover && mousedown) {
             range = e.offsetY - position;
-            const newChange = Math.round(range / -10);
-            if(newChange !== change){
+            const newChange = Math.round(range / -5);
+            if (newChange !== change) {
                 let temperature = +temperatureInfo.innerText;
-                if(newChange < change){
+                if (newChange < change) {
                     temperature = temperature - 1;
                 } else {
                     temperature = temperature + 1;
@@ -192,7 +217,7 @@ function changeTemperature() {
                 change = newChange;
                 renderTemperature(temperature);
             }
-            
+
         }
     }
 }
@@ -206,31 +231,101 @@ temperatureSaveButton.addEventListener('click', () => {
 temperaturePowerButton.addEventListener("click", () => {
     const power = temperaturePowerButton;
     power.classList.toggle('off');
-    if (power.matches('.off')){
+    if (power.matches('.off')) {
         roomsData[activeRoom].temperatureOff = true;
-    } else{
+    } else {
         roomsData[activeRoom].temperatureOff = false;
     }
 })
-function setTemperaturePower(){
-    if(roomsData[activeRoom].temperatureOff){
+function setTemperaturePower() {
+    if (roomsData[activeRoom].temperatureOff) {
         temperaturePowerButton.classList.add('off');
     } else {
         temperaturePowerButton.classList.remove('off');
     }
-    
+
 }
 
-settingsTabs.querySelectorAll('.tab').forEach( tab=> {
+settingsTabs.querySelectorAll('.tab').forEach(tab => {
     tab.addEventListener('click', () => {
-        const selected = settingsTabs.querySelector('.tab.selected');
-        selected.classList.remove('selected');
         const optionType = tab.dataset.type;
-        changeTab(optionType)
+        activeTab = optionType;
+        changeSettigsType(optionType)
     })
 })
 
-function changeTab(optionType) {
-    activeTab = optionType;
+function changeSettigsType(type) {
+    const tabSelected = settingsTabs.querySelector('.tab.selected');
+    const tab = settingsTabs.querySelector(`[data-type=${type}]`);
+    const panelSelected = settingsPanel.querySelector('.selected');
+    const panel = settingsPanel.querySelector(`[data-type="${type}"]`)
+    tabSelected.classList.remove('selected');
+    panelSelected.classList.remove('selected');
     tab.classList.add('selected');
+    panel.classList.add('selected');
 }
+
+function changeSlider(percent, slider) {
+    if (percent >= 0 && percent <= 100) {
+        const { type } = slider.parentElement.parentElement.dataset
+        slider.querySelector("span").innerText = percent;
+        slider.style.height = `${percent}%`;
+        roomsData[activeRoom][type] = percent;
+    }
+}
+
+function watchSlider(slider) {
+    let mouseover = false;
+    let mousedown = false;
+    let position = 0;
+    let range = 0;
+    let change = 0;
+    const parent = slider.parentElement;
+    parent.onmouseover = () => {
+        mouseover = true;
+        mousedown = false;
+    };
+    parent.onmouseout = () => mouseover = false;
+    parent.onmouseup = (e) => mousedown = false;
+    parent.onmousedown = (e) => {
+        mousedown = true;
+        position = e.offsetY;
+        range = 0;
+    }
+    parent.onmousemove = (e) => {
+        if (mouseover && mousedown) {
+            range = e.offsetY - position;
+            const newChange = Math.round(range / -2);
+            if (newChange !== change) {
+                let percent = +slider.querySelector('span').innerText;
+                if (newChange < change) {
+                    percent = percent - 1;
+                } else {
+                    percent = percent + 1;
+                }
+                change = newChange;
+                changeSlider(percent, slider);
+            }
+
+        }
+    }
+}
+watchSlider(sliders.lights);
+watchSlider(sliders.humidity);
+
+function changeSwitch(activeTab, isOff) {
+    if(isOff) {
+        swtch[activeTab].classList.add("off");
+    } else{
+        swtch[activeTab].classList.remove("off");
+    }
+    roomsData[activeRoom][`${activeTab}Off`] = isOff;
+}
+swtch.humidity.addEventListener("click", () => {
+    const isOff = !swtch.humidity.matches(".off");
+    changeSwitch(activeTab, isOff);
+})
+swtch.lights.addEventListener("click", () => {
+    const isOff = !swtch.lights.matches(".off");
+    changeSwitch(activeTab, isOff);
+})
